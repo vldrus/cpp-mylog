439f123521180ffa2d91dcc170742f870cb11975
Vlad <65050323+vldrus@users.noreply.github.com>
2022-10-06 23:00:37 +0300

    Update mylog.h

--- mylog.h
+++ mylog.h
@@ -1,16 +1,14 @@
 /* Licensed under the MIT License */
 
-#ifndef MYLOG_H
-#define MYLOG_H
+#ifndef _MYLOG_H
+#define _MYLOG_H 1
 
 #include <ctime>
-#include <chrono>
 #include <sstream>
-#include <iomanip>
 #include <iostream>
 
 #if defined(_WIN32)
-namespace mylog_w
+namespace mywin
 {
     extern "C" __declspec(dllimport) unsigned long __stdcall GetCurrentThreadId();
 }
@@ -25,34 +23,16 @@ class myout
 public:
     ~myout()
     {
-        std::cout << oss.str() + '\n';
+        std::cout << m_stream.str() + '\n';
     }
 
     std::ostringstream &stream()
     {
-        return oss;
+        return m_stream;
     }
 
 private:
-    std::ostringstream oss;
-};
-
-/* Internal class for myerr macros */
-class myerr
-{
-public:
-    ~myerr()
-    {
-        std::cerr << oss.str() + '\n';
-    }
-
-    std::ostringstream &stream()
-    {
-        return oss;
-    }
-
-private:
-    std::ostringstream oss;
+    std::ostringstream m_stream;
 };
 
 /* Internal class for mylog macros */
@@ -61,11 +41,10 @@ class mylog
 public:
     mylog()
     {
-        auto now = std::chrono::system_clock::now();
-        auto now_time = std::chrono::system_clock::to_time_t(now);
-        auto now_millis = std::chrono::duration_cast<std::chrono::milliseconds>(now.time_since_epoch()).count();
+        std::tm now_tm = std::tm();
+        std::time_t now_time = std::time_t();
 
-        auto now_tm = std::tm();
+        std::time(&now_time);
 
 #if defined(_WIN32)
         ::localtime_s(&now_tm, &now_time);
@@ -73,43 +52,37 @@ public:
         ::localtime_r(&now_time, &now_tm);
 #endif
 
-#if defined(_WIN32)
-        auto thread_id = mylog_w::GetCurrentThreadId();
-#else
-        auto thread_id = ::syscall(SYS_gettid);
-#endif
+        char now_str[20];
+        std::strftime(now_str, 20, "%Y-%m-%d %H:%M:%S", &now_tm);
 
-        oss << std::put_time(&now_tm, "%Y-%m-%d")
-            << " "
-            << std::put_time(&now_tm, "%H:%M:%S")
-            << "."
-            << std::setw(3) << std::setfill('0') << (now_millis % 1000)
-            << " [ "
-            << thread_id
-            << " ] ";
+        m_stream
+            << now_str
+            << ' '
+#if defined(_WIN32)
+            << mywin::GetCurrentThreadId() << ' ';
+#elif defined(SYS_gettid)
+            << ::syscall(SYS_gettid) << ' ';
+#endif
     }
 
     ~mylog()
     {
-        std::cerr << oss.str() + '\n';
+        std::cerr << m_stream.str() + '\n';
     }
 
     std::ostringstream &stream()
     {
-        return oss;
+        return m_stream;
     }
 
 private:
-    std::ostringstream oss;
+    std::ostringstream m_stream;
 };
 
 /* Print to stdout with a newline. Usage: myout << "Hello"; */
 #define myout myout().stream()
 
-/* Print to stderr with a newline. Usage: myerr << "Hello"; */
-#define myerr myerr().stream()
-
 /* Log to stderr with date & time. Usage: mylog << "Hello"; */
 #define mylog mylog().stream()
 
-#endif /* MYLOG_H */
+#endif /* _MYLOG_H */
