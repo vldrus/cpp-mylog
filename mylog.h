1f17e6f1ce190048d3e9ff1b9d62e3990db4b17a
Vlad <65050323+vldrus@users.noreply.github.com>
2022-10-08 17:24:50 +0300

    Update mylog.h

--- mylog.h
+++ mylog.h
@@ -1,26 +1,41 @@
+/* Licensed under the MIT License */
+
 #ifndef _MYLOG_H
-#define _MYLOG_H
+#define _MYLOG_H 1
 
 #include <ctime>
 #include <sstream>
 #include <iostream>
 
-#if !defined(_WIN32)
+#if defined(_WIN32)
+namespace mywin
+{
+    extern "C" __declspec(dllimport) unsigned long __stdcall GetCurrentThreadId();
+}
+#else
 #include <unistd.h>
 #include <sys/syscall.h>
 #endif
 
+/* Internal class for myout macros */
 class myout
 {
 public:
-    ~myout() { std::cout << m_stream.str() + "\n"; }
+    ~myout()
+    {
+        std::cout << m_stream.str() + "\n";
+    }
 
-    std::ostringstream &stream() { return m_stream; }
+    std::ostringstream &stream()
+    {
+        return m_stream;
+    }
 
 private:
     std::ostringstream m_stream;
 };
 
+/* Internal class for mylog macros */
 class mylog
 {
 public:
@@ -30,7 +45,9 @@ public:
 
         std::tm now_tm = std::tm();
 
-#if defined(_WIN32)
+#if defined(_WIN32) && defined(__BORLANDC__)
+        ::localtime_s(&now, &now_tm);
+#elif defined(_WIN32)
         ::localtime_s(&now_tm, &now);
 #else
         ::localtime_r(&now, &now_tm);
@@ -41,20 +58,31 @@ public:
 
         m_stream << datetime << " ";
 
-#if defined(SYS_gettid)
+#if defined(_WIN32)
+        m_stream << mywin::GetCurrentThreadId() << " ";
+#elif defined(SYS_gettid)
         m_stream << ::syscall(SYS_gettid) << " ";
 #endif
     }
 
-    ~mylog() { std::cout << m_stream.str() + "\n"; }
+    ~mylog()
+    {
+        std::cout << m_stream.str() + "\n";
+    }
 
-    std::ostringstream &stream() { return m_stream; }
+    std::ostringstream &stream()
+    {
+        return m_stream;
+    }
 
 private:
     std::ostringstream m_stream;
 };
 
+/* Print to stdout with a newline. Usage: myout << "Hello"; */
 #define myout myout().stream()
+
+/* Log to stderr with date & time. Usage: mylog << "Hello"; */
 #define mylog mylog().stream()
 
-#endif
+#endif /* _MYLOG_H */
